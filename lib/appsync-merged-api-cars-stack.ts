a89b7f8ad1fa704d6e9636b5004d775e0cc09461
Ruben Rangel <rubenmrangel@gmail.com>
2024-05-05 15:33:55 -0700

    add merged sub APIs for Car Assembly and Parts Manufacturing

--- lib/appsync-merged-api-cars-stack.ts
+++ lib/appsync-merged-api-cars-stack.ts
@@ -6,10 +6,29 @@ export class AppsyncMergedApiCarsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const carAssemblyApi = new appsync.GraphqlApi(this, 'car-manufacturing-api', {
+      name: 'Car Assembly API',
+      definition: appsync.Definition.fromFile('car-manufacturing.schema.graphql'),
+    });
+
+    const partsManufacturingApi = new appsync.GraphqlApi(this, 'parts-manufacturing-api', {
+      name: 'Parts Manufacturing API',
+      definition: appsync.Definition.fromFile('parts-assembly.schema.graphql'),
+    });
+
     new appsync.GraphqlApi(this, 'wheely-api', {
       name: 'Wheely API',
       definition: appsync.Definition.fromSourceApis({
-        sourceApis: [],
+        sourceApis: [
+          {
+            sourceApi: carAssemblyApi,
+            mergeType: appsync.MergeType.AUTO_MERGE,
+          },
+          {
+            sourceApi: partsManufacturingApi,
+            mergeType: appsync.MergeType.AUTO_MERGE,
+          },
+        ],
       }),
     });
   }
